d74c2fa82931145f384f8a6fc41e7ff3b7912d8b
vstaichi <56064137+vstaichi@users.noreply.github.com>
2024-12-23 16:22:12 +0800

    Initialize Provider-setting-refactoring

---
+++ app/types/model.ts
@@ -0,0 +1,11 @@
+import type { ModelInfo } from '~/utils/types';
+
+export type ProviderInfo = {
+  staticModels: ModelInfo[];
+  name: string;
+  getDynamicModels?: (apiKeys?: Record<string, string>) => Promise<ModelInfo[]>;
+  getApiKeyLink?: string;
+  labelForGetApiKey?: string;
+  icon?: string;
+  isEnabled?: boolean;
+};
