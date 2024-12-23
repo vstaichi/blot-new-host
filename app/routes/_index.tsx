ec7125e995a9297331ccaff3df64fd7cf3d5a9d7
vstaichi <56064137+vstaichi@users.noreply.github.com>
2024-12-23 16:22:04 +0800

    Initialize deploy13november

---
+++ app/routes/_index.tsx
@@ -0,0 +1,20 @@
+import { json, type MetaFunction } from '@remix-run/cloudflare';
+import { ClientOnly } from 'remix-utils/client-only';
+import { BaseChat } from '~/components/chat/BaseChat';
+import { Chat } from '~/components/chat/Chat.client';
+import { Header } from '~/components/header/Header';
+
+export const meta: MetaFunction = () => {
+  return [{ title: 'Free Bolt.MyAiBuilt.APP' }, { name: 'description', content: 'Free AI Web App Builder – No Setup Needed!' }];
+};
+
+export const loader = () => json({});
+
+export default function Index() {
+  return (
+    <div className="flex flex-col h-full w-full">
+      <Header />
+      <ClientOnly fallback={<BaseChat />}>{() => <Chat />}</ClientOnly>
+    </div>
+  );
+}
